a27ec7a7f99ff5bd8eb25a71b96bbcd83aa89ad0
aryamanpuri <aryamanpuri@gmail.com>
2020-05-26 12:30:08 +0530

    Added Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { VideoViewComponent } from './video-view/video-view.component';
+import { HistoryComponent } from './history/history.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    VideoViewComponent,
+    HistoryComponent,
+    BookmarksComponent
   ],
   imports: [
     BrowserModule,
